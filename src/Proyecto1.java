8eda3e6f7f6c3a3a9edc6caccf5eac23c5c33b0f
KirilSPan <99761048+KirilSPan@users.noreply.github.com>
2023-11-06 20:48:32 +0100

    Commit 4

--- src/Proyecto1.java
+++ src/Proyecto1.java
@@ -1,3 +1,6 @@
+
+import java.io.File;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -15,7 +18,12 @@ public class Proyecto1 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("Hola Mundo");
+        AccesoDOM accesoLibro = new AccesoDOM();
+        File fichero = new File("./src/books.xml");
+        
+        accesoLibro.abrirXMLaDOM(fichero);
+        
+        
     }
     
 }
