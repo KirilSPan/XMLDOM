9dd0b62d6390d9ed98341a1de9bf23f9eea8066d
KirilSPan <99761048+KirilSPan@users.noreply.github.com>
2023-11-06 21:15:10 +0100

    Commit 8

--- src/AccesoDOM.java
+++ src/AccesoDOM.java
@@ -45,7 +45,6 @@ public class AccesoDOM {
             doc = builder.parse(f);
             // ahora doc apunta al arbol DOM y podemos recorrerlo
             System.out.println("DOM creado con éxito.");
-            
 
             return 0;
 
@@ -53,8 +52,6 @@ public class AccesoDOM {
 
             return -1;
         }
-        
-        
 
     }
 
@@ -107,22 +104,22 @@ public class AccesoDOM {
 
     }
 
-    public int insertarLibroEnDOM(String titulo, String autor, String fecha) {
+    public int insertarLibroEnDOM(String author, String title, String genre) {
         try {
-            System.out.println("Añadir libro al árbol DOM:" + titulo + ";" + autor + ";" + fecha);
+            System.out.println("Add book to main DOM:" + title + ";" + title + ";" + genre);
             //crea los nodos=>los añade al padre desde las hojas a la raíz
             //CREATE TITULO con el texto en medio
-            Node ntitulo = doc.createElement("Titulo");//crea etiquetas <Titulo>...</Titulo>
-            Node ntitulo_text = doc.createTextNode(titulo);//crea el nodo texto para el Titulo
+            Node ntitulo = doc.createElement("Title");//crea etiquetas <Titulo>...</Titulo>
+            Node ntitulo_text = doc.createTextNode(title);//crea el nodo texto para el Titulo
             ntitulo.appendChild(ntitulo_text);//añade el titulo a las etiquetas=><Titulo>titulo</Titulo>
             //Node nautor=doc.createElement("Autor").appendChild(doc.createTextNode(autor));//one line doesn't work
             //CREA AUTOR
-            Node nautor = doc.createElement("Autor");
-            Node nautor_text = doc.createTextNode(autor);
+            Node nautor = doc.createElement("Author");
+            Node nautor_text = doc.createTextNode(title);
             nautor.appendChild(nautor_text);
             //CREA LIBRO, con atributo y nodos Título y Autor 
             Node nLibro = doc.createElement("Libro");
-            ((Element) nLibro).setAttribute("publicado", fecha);
+            ((Element) nLibro).setAttribute("publicado", genre);
             nLibro.appendChild(ntitulo);
             nLibro.appendChild(nautor);
             //APPEND LIBRO TO THE ROOT
@@ -137,8 +134,7 @@ public class AccesoDOM {
             System.out.println(e);
             return -1;
         }
-        
-        
+
     }
 
     @SuppressWarnings("CallToPrintStackTrace")
@@ -163,8 +159,6 @@ public class AccesoDOM {
             System.out.println("Nodo borrado");
             //Guardar el arbol DOM en un nuevo archivo para mantener nuestro archivo original
             //guardarDOMcomoArchivo("LibrosBorrados.xml");
-            
-            
 
             return 0;
         } catch (DOMException e) {
