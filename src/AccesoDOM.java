4b7e0629f475727f5778256e0e697fc252dbbe97
KirilSPan <99761048+KirilSPan@users.noreply.github.com>
2023-11-08 00:50:47 +0100

    commit 12

--- src/AccesoDOM.java
+++ src/AccesoDOM.java
@@ -1,6 +1,13 @@
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.invoke.MethodType;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -55,17 +62,21 @@ public class AccesoDOM {
 
     }
 
-    public void recorrerDOM() {
+    public ArrayList recorrerDOM() {
+
+        String[] datos = null;
+        ArrayList iDs = new ArrayList();
+
         try {
             //codigo escrito en Practica1
             //añade el nuevo método
 
-            String[] datos = new String[7];//lo usamos para la información de cada libro
+            datos = new String[7];//lo usamos para la información de cada libro
             @SuppressWarnings("UnusedAssignment")
             Node nodo = null;
             Node root = doc.getFirstChild();
             NodeList nodelist = root.getChildNodes(); //(1)Ver dibujo del árbol
-            System.out.println(doc.getFirstChild().getNodeName()+ " of Books");
+            System.out.println(doc.getFirstChild().getNodeName() + " of Books");
 
             //recorrer el árbol DOM. El 1er nivel de nodos hijos del raíz
             for (int i = 0; i < nodelist.getLength(); i++) {
@@ -93,8 +104,9 @@ public class AccesoDOM {
                         //el array de String datos[] tiene los valores que necesitamos
 
                     }
-                    
+
                     System.out.println("id: " + datos[0] + " -- Autor: " + datos[1] + " -- Título: " + datos[2] + " -- Género: " + datos[3] + " -- Precio: " + datos[4] + " -- Año publicación: " + datos[5] + " -- Descripción: " + datos[6]);
+                    iDs.add(datos[0]);
 
                 }
             }
@@ -102,13 +114,111 @@ public class AccesoDOM {
             System.out.println(e.toString());
 
         }
+
+        return iDs;
     }
 
-    public int insertarLibroEnDOM(String author, String title, String genre) {
+    public int insertarLibroEnDOM(String author, String title, String genre, double price, String publish_date, String description) {
         try {
-            System.out.println("Add book to main DOM:" + title + ";" + title + ";" + genre);
+            System.out.println("Add book to main DOM:" + author + ";" + title + ";" + genre);
+
+            ArrayList<String> retrievedData = recorrerDOM(); // Assuming the method is called within the same class or accessible
+
+            String datePattern = "yyyy-MM-dd"; // Date pattern to parse the publish_date
+            SimpleDateFormat dateFormat = new SimpleDateFormat(datePattern);
+            Date formattedDate = dateFormat.parse(publish_date);
+
+            String incrementedID = null;
+
+            // Get the last ID from the retrieved ArrayList
+            if (!retrievedData.isEmpty()) {
+                String lastIDString = retrievedData.get(retrievedData.size() - 1);
+
+                // Increment the last ID
+                int lastID;
+                try {
+                    lastID = Integer.parseInt(lastIDString.replaceAll("[^0-9]", ""));
+                    lastID++; // Increment the ID by 1
+                    incrementedID = String.valueOf(lastID);
+                    System.out.println("Incremented ID: " + incrementedID);
+                } catch (NumberFormatException e) {
+                    System.out.println("Last ID is not a valid integer.");
+                }
+            } else {
+                System.out.println("No IDs found in the ArrayList.");
+            }
+
+            Node nauthor = doc.createElement("author");
+            Node nauthor_text = doc.createTextNode(author);
+            nauthor.appendChild(nauthor_text);
+
+            Node ntitle = doc.createElement("title");
+            Node ntitle_text = doc.createTextNode(title);
+            ntitle.appendChild(ntitle_text);
+
+            Node ngenre = doc.createElement("genre");
+            Node ngenre_text = doc.createTextNode(genre);
+            ngenre.appendChild(ngenre_text);
+
+            Node nprice = doc.createElement("price");
+            Node nprice_text = doc.createTextNode(Double.toString(price));
+            nprice.appendChild(nprice_text);
+
+            Node npublish_date = doc.createElement("publish_date");
+            Node npublish_date_text = doc.createTextNode(dateFormat.format(formattedDate));
+            npublish_date.appendChild(npublish_date_text);
+
+            Node ndescription = doc.createElement("description");
+            Node ndescription_text = doc.createTextNode(description);
+            ndescription.appendChild(ndescription_text);
+
+            Node newBook = doc.createElement("book");
+            ((Element) newBook).setAttribute("id", "bk" + incrementedID);
+            newBook.appendChild(nauthor);
+            newBook.appendChild(ntitle);
+            newBook.appendChild(ngenre);
+            newBook.appendChild(nprice);
+            newBook.appendChild(npublish_date);
+            newBook.appendChild(ndescription);
+
+            newBook.appendChild(doc.createTextNode("\n"));
+
+            Node raiz = doc.getFirstChild();
+            raiz.appendChild(newBook);
+            System.out.println("Libro insertado en DOM.");
+            return 0;
+        } catch (DOMException | ParseException e) {
+            System.out.println(e);
+            return -1;
+        }
+    }
+
+    public int insertarLibroEnDOMv(String author, String title, String genre, double price, String publish_date, String description) {
+        try {
+            System.out.println("Add book to main DOM:" + author + ";" + title + ";" + genre);
             //crea los nodos=>los añade al padre desde las hojas a la raíz
-            //CREATE TITULO con el texto en medio
+            //CREATE TITULO con el texto en medio 
+
+            ArrayList<String> retrievedData = recorrerDOM(); // Assuming the method is called within the same class or accessible
+
+            // Get the last ID from the retrieved ArrayList
+            if (!retrievedData.isEmpty()) {
+                String lastIDString = retrievedData.get(retrievedData.size() - 1);
+
+                // Increment the last ID
+                int lastID;
+                try {
+                    lastID = Integer.parseInt(lastIDString);
+                    lastID++; // Increment the ID by 1
+                    String incrementedID = String.valueOf(lastID);
+                    System.out.println("Incremented ID: " + incrementedID);
+                } catch (NumberFormatException e) {
+                    System.out.println("Last ID is not a valid integer.");
+                }
+            } else {
+                System.out.println("No IDs found in the ArrayList.");
+            }
+
             Node ntitulo = doc.createElement("Title");//crea etiquetas <Titulo>...</Titulo>
             Node ntitulo_text = doc.createTextNode(title);//crea el nodo texto para el Titulo
             ntitulo.appendChild(ntitulo_text);//añade el titulo a las etiquetas=><Titulo>titulo</Titulo>
--- src/Proyecto1.java
+++ src/Proyecto1.java
@@ -1,11 +1,12 @@
 
 import java.io.File;
+import java.util.ArrayList;
+
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
 /**
  *
  * @author Kiril_SP
@@ -17,20 +18,20 @@ public class Proyecto1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         AccesoDOM accesoLibro = new AccesoDOM();
         File fichero = new File("./src/books.xml");
-        
+
         accesoLibro.abrirXMLaDOM(fichero);
         accesoLibro.recorrerDOM();
         
+        accesoLibro.insertarLibroEnDOM("Kiril", "His First Book", "Comedy", 12.42, "2023-11-08", "The description is not present.");
+        
+        accesoLibro.recorrerDOM();
+
         accesoLibro.deleteNode("Midnight Rain");
         accesoLibro.recorrerDOM();
         accesoLibro.guardarDOMcomoArchivo("./src/newBooks.xml");
-        
-        
-        
-        
     }
-    
+
 }
